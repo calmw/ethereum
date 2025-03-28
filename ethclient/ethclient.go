3edcf28250782cb423c51d3c59fd073b44530346
calm <calm@calm.com>
2025-03-29 03:50:57 +0800

    1

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -303,10 +303,7 @@ func (ec *Client) getBlock(ctx context.Context, method string, args ...interface
 			Withdrawals:           layerTwoBlock.Withdrawals,
 			WithdrawalsRoot:       layerTwoBlock.WithdrawalsRoot,
 		}
-		//fmt.Println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", layerTwoBlock.Size)
 		layerTwoBlockNew.Transactions = make([]LayerTwoTxDataNew, len(layerTwoBlock.Transactions))
-		//layerTwoBlockNew.Size = "0x00"
-		//layerTwoBlockNew.Transactions = make([]LayerTwoTxDataNew, 0)
 		for i, t := range layerTwoBlock.Transactions {
 			layerTwoBlockNew.Transactions[i] = LayerTwoTxDataNew{
 				BlockHash:            t.BlockHash,
